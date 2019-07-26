39db4a56f8f5f2171f8eadbc8457d4808c4c3c84
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-25 23:52:35 -0400

    Initial setup with Redux and thunk

---
+++ books/src/Components/Dictionary.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Dictionary = props => {
+    return (
+        <div>
+            <h2>Search here</h2>
+                <input></input>
+            <button>Search</button>
+        </div>
+    )
+}
+
+export default Dictionary;
---
+++ books/src/store/Reducers/index.js
@@ -0,0 +1,13 @@
+export const initialState = {
+    isFetching: false,
+    error: '',
+    jokes: []
+}
+
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state;
+    }
+}
