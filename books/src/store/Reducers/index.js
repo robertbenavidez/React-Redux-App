3873da1034304c5a972065705d73516190d7e2b0
Robert Benavidez <murabitbenavidez@gmail.com>
2019-07-26 06:49:27 -0400

    completed some logic and passed data to Dictionary.js successfully

--- books/src/store/Reducers/index.js
+++ books/src/store/Reducers/index.js
@@ -1,13 +1,27 @@
+import {
+    FETCH_DICTIONARY_START,
+    FETCH_DICTIONARY_SUCCESS,
+    FETCH_DICTIONARY_START_FAILURE
+} from '../actions/index';
+
+
+
 export const initialState = {
-    isFetching: false,
     error: '',
-    jokes: []
-}
+    isFetching: false,
+    dictionary: null
+};
 
 
 export const reducer = (state = initialState, action) => {
     switch(action.type) {
+        case FETCH_DICTIONARY_START:
+            return {
+                ...state,
+                isFetching: true,
+                dictionary: null
+            }
         default:
             return state;
     }
-}
+};
